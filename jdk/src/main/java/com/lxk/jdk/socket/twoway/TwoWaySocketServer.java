f49bb2b44b11b4c101b1c2c68fbadb8c2199e4f4
李学凯 <cmshome@163.com>
2019-10-22 16:32:05 +0800

    简单测试

--- jdk/src/main/java/com/lxk/jdk/socket/twoway/TwoWaySocketServer.java
+++ jdk/src/main/java/com/lxk/jdk/socket/twoway/TwoWaySocketServer.java
@@ -46,13 +46,15 @@ public class TwoWaySocketServer {
             stringBuilder.append(new String(readBytes, 0, msgLen, "UTF-8"));
         }
 
-        System.out.println("get message from client:       " + stringBuilder);
+        System.out.println("get message from client:");
+        System.out.println(stringBuilder);
         // 告诉客户端接收已经完毕，之后只能发送
         socket.shutdownInput();
 
         // write the receipt.
         OutputStream outputStream = socket.getOutputStream();
         String receipt = "We received your message:[" + stringBuilder.toString() + "] and send it back.";
+        receipt = "00001735<?xml version=\"1.0\" encoding=\"UTF-8\"?><service>    <SysHead>        <SvcCd>60012000016</SvcCd>        <ScnCd>01</ScnCd>        <CnsmrSysNo>CnsmrSysNo</CnsmrSysNo>        <CnsmrSrlNo>CnsmrSrlNo</CnsmrSrlNo>        <BsnSrlNo>CnsmrSrlNo</BsnSrlNo>        <TxnDt>20190821</TxnDt>        <TxnTm>163045</TxnTm>        <PvdrSysNo>501500</PvdrSysNo>        <PvdrSrlNo>500102019082100001999</PvdrSrlNo>        <RetSt>S</RetSt>        <RetInfArry>            <RetCd>000000</RetCd>            <RetMsg>校验成功</RetMsg>        </RetInfArry>        <OrgnlCnsmrSysNo/>        <OrgnlTmlIndNo/>        <TmlIndNo/>        <OrgnlCnsmrSvcNo/>        <CnsmrSvcNo/>        <PvdrSvcNo/>        <UsrLng/>        <FileFlg/>        <CnlTp/>        <CnlDtlTp/>        <MACVal/>        <SvcVerNo/>        <PrtyLvl/>    </SysHead>    <AppHead>        <LglPrsnCd/>        <InstNo/>        <UsrNo/>        <UsrPswd/>        <UsrLvl/>        <UsrTp/>        <TlrSrlNo/>        <ChkFlg/>        <AuthFlg/>        <CnterTxnCd/>        <PblcPreRsrvFld1/>        <PblcPreRsrvFld2/>        <PblcPreRsrvFld3/>    </AppHead>    <Body>        <UsrCd>2671</UsrCd>        <LogonNm>0023090</LogonNm>        <Email/>        <UsrNm>冯延波</UsrNm>        <PstDsc>助审</PstDsc>        <MblNo1>139*****3</MblNo1>        <MblNo2/>        <FixTel>0953-2020188</FixTel>        <EmplyWrkNo>0023090</EmplyWrkNo>        <DeptCd>0015025</DeptCd>        <DeptNo>241</DeptNo>        <RlCd>1</RlCd>        <BlngArea/>        <CommNo>4</CommNo>        <FbdUseFlg>0</FbdUseFlg>        <DelFlg>0</DelFlg>        <MblShrtNo/>        <CommNm>黄河银行</CommNm>        <PhtAdr>https://mobileoa.bankyellowriver.com/photos/f22fc04208c1a2d4d07fffb2fec98d5e</PhtAdr>    </Body></service>";
         outputStream.write(receipt.getBytes("UTF-8"));
 
         inputStream.close();
