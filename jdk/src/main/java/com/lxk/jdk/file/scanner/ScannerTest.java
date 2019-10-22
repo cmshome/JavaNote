fe2de9de5211f7d5055848077157bac68b21ef17
李学凯 <cmshome@163.com>
2019-10-22 10:50:23 +0800

    使用scanner读个xml文件。

--- jdk/src/main/java/com/lxk/jdk/file/scanner/ScannerTest.java
+++ jdk/src/main/java/com/lxk/jdk/file/scanner/ScannerTest.java
@@ -1,11 +1,18 @@
 package com.lxk.jdk.file.scanner;
 
 import com.google.common.collect.Lists;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,6 +22,62 @@ import java.util.Scanner;
  * @author LiXuekai on 2019/9/11
  */
 public class ScannerTest {
+
+
+    public static void main(String[] args) {
+        String s = readFile3("/Users/fang/Downloads/request.xml");
+
+        SAXReader reader = new SAXReader();
+        reader.setEncoding("utf-8");
+        try {
+            //Document read = reader.read(new ByteArrayInputStream(s.getBytes("utf-8")));
+            Document read = DocumentHelper.parseText(s);
+
+            Element rootElement = read.getRootElement();
+            Element body = rootElement.element("Body");
+            System.out.println(body.element("SsoToken").getStringValue());
+            body.addAttribute("SsoToken","123");
+            System.out.println(body.element("SsoToken").getStringValue());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+        String bitLength = get8BitLength(bytes.length + "");
+        System.out.println(bitLength + s);
+
+    }
+    private static String readFile3(String path) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (Scanner scanner = new Scanner(new File(path), StandardCharsets.UTF_8.name())) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                stringBuilder.append(line);
+            }
+        } catch (Exception e) {
+        }
+        return stringBuilder.toString();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static String get8BitLength(String length) {
+        int expectLength = 8;
+        int count = length.length();
+        int needAdd = expectLength - count;
+        StringBuilder stringBuilder = new StringBuilder();
+        List<String> list = Lists.newArrayList();
+        if (needAdd > 0) {
+            list = new ArrayList(Collections.nCopies(needAdd, "0"));
+        }
+        if (!list.isEmpty()) {
+            list.forEach(stringBuilder::append);
+        }
+        String s = stringBuilder.toString() + length;
+        System.out.println(s);
+        return s;
+    }
 
     @Test
     public void read() {
