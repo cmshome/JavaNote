786e3a44e8b3164612129ae65f646d7915ce0509
李学凯 <cmshome@163.com>
2020-04-02 09:37:55 +0800

    空list使用get(index)是出IndexOutOfBoundsException异常的记录

--- jdk/src/main/java/com/lxk/jdk/collection/EmptyList.java
+++ jdk/src/main/java/com/lxk/jdk/collection/EmptyList.java
@@ -1,5 +1,6 @@
 package com.lxk.jdk.collection;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -28,6 +29,21 @@ public class EmptyList {
             System.out.println(e.getMessage());
         }
 
+    }
+
+    @Test
+    public void test(){
+        List<String> list = Lists.newArrayList();
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        // IndexOutOfBoundsException 异常
+        String s = list.get(0);
     }
 }
 
