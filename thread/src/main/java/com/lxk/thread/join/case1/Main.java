3d9e9062738c206908d5589790b46485f7bdc9b5
李学凯 <cmshome@163.com>
2020-04-29 16:26:06 +0800

    放在一起方便看

--- thread/src/main/java/com/lxk/thread/join/case1/Main.java
+++ thread/src/main/java/com/lxk/thread/join/case1/Main.java
@@ -15,3 +15,42 @@ public class Main {
         thread2.start();
     }
 }
+
+class Thread1 extends Thread {
+    public Thread1(String threadName) {
+        super(threadName);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() + " is running");
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(getName() + " is running over");
+
+    }
+}
+
+class Thread2 extends Thread {
+    private Thread1 thread1;
+
+    public Thread2(String threadName, Thread1 thread1) {
+        super(threadName);
+        this.thread1 = thread1;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() + " is running");
+        try {
+            thread1.start();
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("thread2 is over");
+    }
+}
--- thread/src/main/java/com/lxk/thread/join/case2/Main.java
+++ thread/src/main/java/com/lxk/thread/join/case2/Main.java
@@ -22,3 +22,24 @@ public class Main {
         t2.start();
     }
 }
+
+class ThreadTest implements Runnable {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(this.getName() + "------" + i);
+        }
+    }
+
+}
