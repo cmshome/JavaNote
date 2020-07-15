b93dbfc0cb687837b85179871b6a53c9b4d1382e
李学凯 <cmshome@163.com>
2020-07-15 11:15:53 +0800

    http test

---
+++ jdk/src/main/java/com/lxk/jdk/http/HttpTest.java
@@ -0,0 +1,29 @@
+package com.lxk.jdk.http;
+
+import com.google.common.collect.Maps;
+import com.lxk.tool.HttpUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * http test
+ *
+ * @author LiXuekai on 2020/7/15
+ */
+public class HttpTest {
+
+    /**
+     * post 带参数
+     */
+    @Test
+    public void once() throws IOException {
+        String url = "http://192.168.100.127:8088/refresh";
+        Map<String, String> paramMap = Maps.newHashMapWithExpectedSize(2);
+        paramMap.put("type", "AsyncStream");
+        HttpUtils.HttpClientResult httpClientResult = HttpUtils.doPost(url, paramMap);
+        System.out.println(httpClientResult.getCode());
+        System.out.println(httpClientResult.getContent());
+    }
+}
