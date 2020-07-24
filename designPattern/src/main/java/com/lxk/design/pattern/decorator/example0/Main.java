8944d6e32f117391389f0b2f4e7a3de874128974
李学凯 <cmshome@163.com>
2020-07-24 10:39:02 +0800

    装饰器模式（Decorator Pattern）允许向一个现有的对象添加新的功能，同时又不改变其结构。

--- designPattern/src/main/java/com/lxk/design/pattern/decorator/example0/Main.java
+++ designPattern/src/main/java/com/lxk/design/pattern/decorator/example0/Main.java
@@ -33,8 +33,10 @@ public class Main {
         Shape circle = new Circle();
         ShapeDecorator redCircle = new RedShapeDecorator(new Circle());
         ShapeDecorator redRectangle = new RedShapeDecorator(new Rectangle());
+
         //Shape redCircle = new RedShapeDecorator(new Circle());
         //Shape redRectangle = new RedShapeDecorator(new Rectangle());
+
         System.out.println("Circle with normal border");
         circle.draw();
 
