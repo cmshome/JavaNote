d2d678c82c78d19df0842a1e719b158792b5dbaa
李学凯 <cmshome@163.com>
2020-08-03 11:20:16 +0800

    十大经典排序算法

---
+++ sorting/src/main/java/com/lxk/sort/BubbleSort.java
@@ -0,0 +1,43 @@
+package com.lxk.sort;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ *
+ * @author LiXuekai on 2020/8/3
+ */
+public class BubbleSort {
+    @Test
+    public void test() {
+
+    }
+
+    public int[] sort(int[] sourceArray) {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        for (int i = 1; i < arr.length; i++) {
+            // 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成。
+            boolean flag = true;
+
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+
+                    flag = false;
+                }
+            }
+
+            if (flag) {
+                break;
+            }
+        }
+        return arr;
+
+    }
+}
---
+++ sorting/src/main/java/com/lxk/sort/ShellSort.java
@@ -0,0 +1,39 @@
+package com.lxk.sort;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 希尔排序
+ *
+ * @author LiXuekai on 2020/8/3
+ */
+public class ShellSort {
+
+    @Test
+    public int[] sort(int[] sourceArray) throws Exception {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        int gap = 1;
+        while (gap < arr.length) {
+            gap = gap * 3 + 1;
+        }
+
+        while (gap > 0) {
+            for (int i = gap; i < arr.length; i++) {
+                int tmp = arr[i];
+                int j = i - gap;
+                while (j >= 0 && arr[j] > tmp) {
+                    arr[j + gap] = arr[j];
+                    j -= gap;
+                }
+                arr[j + gap] = tmp;
+            }
+            gap = (int) Math.floor(gap / 3);
+        }
+
+        return arr;
+    }
+}
