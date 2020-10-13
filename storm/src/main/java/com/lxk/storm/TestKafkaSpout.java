e71f91eec67397a7772526560ec130b30f98270f
李学凯 <cmshome@163.com>
2020-10-13 14:35:58 +0800

    kafka spout 消费的实例

---
+++ storm/src/main/java/com/lxk/storm/TestKafkaSpout.java
@@ -0,0 +1,67 @@
+package com.lxk.storm;
+
+import com.lxk.storm.bolt.OutInfoBolt;
+import org.apache.kafka.common.utils.Utils;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.kafka.spout.KafkaSpout;
+import org.apache.storm.kafka.spout.KafkaSpoutConfig;
+import org.apache.storm.topology.TopologyBuilder;
+import org.junit.Test;
+
+/**
+ * 就测试一下从kafka消费数据，然后在bolt里面可以看到数据。
+ *
+ * @author LiXuekai on 2020/10/13
+ */
+public class TestKafkaSpout {
+    private static final String TOPOLOGY_NAME = "kafka-spout-topology";
+
+    @Test
+    public void test() {
+
+        //1，创建了一个TopologyBuilder实例
+        TopologyBuilder topologyBuilder = new TopologyBuilder();
+
+        //1，实例化 kafka spout
+        kafkaSpout(topologyBuilder);
+
+        //3，bolt 消费数据
+        outInfoBolt(topologyBuilder);
+
+        //4，提交
+        submitTopology(topologyBuilder);
+    }
+
+    private void submitTopology(TopologyBuilder builder) {
+        //Config类是一个HashMap<String,Object>的子类，用来配置topology运行时的行为
+        Config config = new Config();
+        //设置worker数量
+        //config.setNumWorkers(2);
+        LocalCluster cluster = new LocalCluster();
+
+        //本地提交
+        cluster.submitTopology(TOPOLOGY_NAME, config, builder.createTopology());
+
+        Utils.sleep(10000);
+        cluster.killTopology(TOPOLOGY_NAME);
+        cluster.shutdown();
+    }
+
+    private void outInfoBolt(TopologyBuilder topologyBuilder) {
+        OutInfoBolt outInfoBolt = new OutInfoBolt();
+        topologyBuilder.setBolt("out-info-bolt-id", outInfoBolt, 1).setNumTasks(1).shuffleGrouping("kafka-info-spout-id");
+
+    }
+
+    private void kafkaSpout(TopologyBuilder topologyBuilder) {
+        KafkaSpout<String, String> kafkaSpout = new KafkaSpout<>(initKafkaSpoutConfig());
+        topologyBuilder.setSpout("kafka-info-spout-id", kafkaSpout, 1);
+    }
+
+    private KafkaSpoutConfig<String, String> initKafkaSpoutConfig() {
+        KafkaSpoutConfig.Builder<String, String> builder = KafkaSpoutConfig.builder("192.168.1.191:9092", "a_citic_test_lxk");
+        builder.setProp("group.id", "KAFKA_STORM");
+        return builder.build();
+    }
+}
---
+++ storm/src/main/java/com/lxk/storm/bolt/OutInfoBolt.java
@@ -0,0 +1,39 @@
+package com.lxk.storm.bolt;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+import java.util.Map;
+
+/**
+ * 接收传的信息，打印
+ *
+ * @author LiXuekai on 2020/10/13
+ */
+public class OutInfoBolt extends BaseRichBolt {
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        try {
+            // [topic, partition, offset, key, value]
+            String string = input.getString(4);
+            System.out.println(string);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+    }
+}
